f6f58cf7679e8a8d4afdfd26b819d100f6e8c2a8
Fuadcsharp <fuadmeherremli.com@gmail.com>
2026-03-05 17:06:41 +0400

    my first backend upload

---
+++ backend/main.py
@@ -0,0 +1,39 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from typing import List
+
+
+app=FastAPI()
+origins=["http://localhost:5173"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_headers=["*"],
+    allow_methods=["*"],
+)
+
+#try fruit:str
+class Fruit(BaseModel):
+    name: str
+
+class Fruits(BaseModel):
+    fruits: List[Fruit]
+
+
+memory_db={"fruits":[]}
+
+
+@app.get("/fruits",response_model=Fruits)
+def get_fruits():
+    return Fruits(fruits=memory_db["fruits"])
+
+@app.post("/fruits",response_model=Fruit)
+def post_fruits(fruit: Fruit):
+    memory_db["fruits"].append(fruit)
+    return fruit
+
+if __name__=="__main__":
+    uvicorn.run(app,host="0.0.0.0",port=8000)
